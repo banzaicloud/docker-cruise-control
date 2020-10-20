b152b611ca543804e6236601e013f664eb6191b1
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2020-10-20 14:30:20 +0200

    feat: set DNS cache property for JVM

--- opt/cruise-control/start.sh
+++ opt/cruise-control/start.sh
@@ -6,4 +6,8 @@ cd /opt/cruise-control
 if [ -z "${KAFKA_HEAP_OPTS}" ]; then
   export KAFKA_HEAP_OPTS="-XX:InitialRAMPercentage=75 -XX:MinRAMPercentage=75 -XX:MaxRAMPercentage=75"
 fi
+
+if [ -z "${KAFKA_OPTS}" ]; then
+  export KAFKA_OPTS="-Dsun.net.inetaddr.ttl=60"
+fi
 /bin/bash ${DEBUG:+-x} /opt/cruise-control/kafka-cruise-control-start.sh /opt/cruise-control/config/cruisecontrol.properties 8090
